d2fd7a27491a0ae7f5ec16ffc5a21177143ad99d
jellymin <2206657169@qq.com>
2019-06-22 18:03:26 +0800

    some changes

--- toolkit/urls.py
+++ toolkit/urls.py
@@ -19,5 +19,8 @@ from toolkitapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', views.index, name='index'),
+    url(r'index/', views.index, name='index'),
+
+
+
 ]
--- toolkitapp/views.py
+++ toolkitapp/views.py
@@ -2,9 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import FileResponse
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def file_down(request):
+    file=open('/home/amarsoft/download/example.tar.gz','rb')
+    response =FileResponse(file)
+    response['Content-Type']='application/octet-stream'
+    response['Content-Disposition']='attachment;filename="example.tar.gz"'
+    return response
